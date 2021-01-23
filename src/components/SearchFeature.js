47cec0933c833fc75181d04ecc9ad9e7bab012ae
AbdulioKhanio <Akhan17@outlook.com>
2021-01-22 18:48:27 -0600

    search functionality added

--- src/components/SearchFeature.js
+++ src/components/SearchFeature.js
@@ -1,14 +1,107 @@
-import React from 'react'
+import React, { Component } from 'react'
+import {Form, Button, Card, CardDeck} from 'react-bootstrap'
+import { connect } from 'react-redux'
+import {getBlogs} from '../actions/blogs'
+import { Link } from 'react-router-dom'
 
-function SearchFeature() {
+class SearchFeature extends Component {
 
+    componentDidMount () {
+        this.props.getBlogs()
+    }
 
+    handleClick = (event) =>{
+        event.preventDefault()
+        alert("hi")
+    }
 
+    render(){
+        const blogs = this.props.blogs.map((blog, i) => {
+            return (
+            <div className="center-card">
+              <div className="py-3 px-3"  >
+                <Card style={{backgroundColor: '#282c34', width: '30rem'}} border="light"  >
+                <link rel="stylesheet" href= "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"></link>
+                  <div key={i} >
+                      <h3>
+                        <div className = "card-text-description-header py-2">
+                          {blog.title + " "}
+                        </div>
+            
+                        <hr/>
+                        <div>
+                          <Link to={`/blogs/${blog.id}/edit` } className = "link" id = {blog.id}><Button variant="success" id = {blog.id}>Edit</Button></Link>
+                          <Link to ={`/blogs/${blog.id}`}><Button variant="info" id = {blog.id} >Read</Button></Link>
+                          <Button variant="danger" id = {blog.id} onClick={this.handleClick}>Delete</Button>
+                          
+                        </div>
+                      </h3> 
+                      <p> 
+                        <div className="card-text-description px-3">
+                          {blog.description} 
+                        </div>
+                      </p>
+                    </div>
+                    
+                </Card> 
+              </div>
+            </div>)
+        })
+
+        const blog = this.props.blogs.filter(blog => blog.title === "Git Pocket Guide").map((blog, i) => {
+            return (
+                <div className="center-card">
+                  <div className="py-3 px-3"  >
+                    <Card style={{backgroundColor: '#282c34', width: '30rem'}} border="light"  >
+                    <link rel="stylesheet" href= "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"></link>
+                      <div key={i} >
+                          <h3>
+                            <div className = "card-text-description-header py-2">
+                              {blog.title + " "}
+                            </div>
+                
+                            <hr/>
+                            <div>
+                              <Link to={`/blogs/${blog.id}/edit` } className = "link" id = {blog.id}><Button variant="success" id = {blog.id}>Edit</Button></Link>
+                              <Link to ={`/blogs/${blog.id}`}><Button variant="info" id = {blog.id} >Read</Button></Link>
+                              <Button variant="danger" id = {blog.id} onClick={this.handleClick}>Delete</Button>
+                              
+                            </div>
+                          </h3> 
+                          <p> 
+                            <div className="card-text-description px-3">
+                              {blog.description} 
+                            </div>
+                          </p>
+                        </div>
+                        
+                    </Card> 
+                  </div>
+                </div>)
+        })
+
+        
+    
     return (
         <div>
             inside search feature
+            <Form>
+                <Form.Control type="search" placeholder="Enter search" />
+                <Button onClick = {this.handleClick} variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+
+            {this.props.loading ? <h3>Loading...</h3> : <CardDeck className="center-card"> {blog} </CardDeck>}
         </div>
-    )
+    )}
 }
 
-export default SearchFeature
+const mapStateToProps = state => {
+    return{
+        blogs: state.blogReducer.blogs,
+        loading: state.blogReducer.loading 
+    }
+}
+
+export default connect(mapStateToProps,{getBlogs})(SearchFeature)
