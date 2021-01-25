4372563644bee3ce2d298590fa43158cbdd6070c
AbdulioKhanio <Akhan17@outlook.com>
2021-01-25 14:53:19 -0600

    --

--- src/components/statefull/Timer.js
+++ src/components/statefull/Timer.js
@@ -26,7 +26,7 @@ class Timer extends React.Component {
     render() {
     return (
         <div>
-        <h1>Hello, world!</h1>
+        <h1>Practice Timer</h1>
         <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
         </div>
     );
