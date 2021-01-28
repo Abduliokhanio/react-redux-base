dc8a14a9cee419a2e5699f4204600ab6240ac9ee
AbdulioKhanio <Akhan17@outlook.com>
2021-01-28 12:40:22 -0600

    --final update :,)

--- src/components/statefull/Blog/BlogsIndex.js
+++ src/components/statefull/Blog/BlogsIndex.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { getBlogs, deleteBlog, editBlog } from '../../../actions/blogs'
 
-import {Button, CardDeck, Card} from 'react-bootstrap'
+import {Button, CardDeck, Card, Form} from 'react-bootstrap'
 
 //link
 import { Link } from 'react-router-dom'
@@ -11,6 +11,11 @@ import { Link } from 'react-router-dom'
 import "../../../index.css"
 
 class BlogsIndex extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {search: {}}
+  }
 
   componentDidMount () {
     this.props.getBlogs()
@@ -21,7 +26,49 @@ class BlogsIndex extends Component {
     this.props.deleteBlog(event.target.id)
   }
 
+  someFunction = (event) => {
+    event.preventDefault()
+    let value = event.target.getElementsByTagName("input")[0].value
+
+    this.setState({search: value}) 
+  }
+
   render() {
+    debugger
+    const blog =  this.props.blogs.filter(blog => blog.title === this.state.search).map((blog, i) => {
+      return (
+          <div className="center-card">
+          <div className="py-3 px-3"  >
+              <Card style={{backgroundColor: '#282c34', width: '30rem'}} border="light"  >
+              <link rel="stylesheet" href= "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"></link>
+              <div key={i} >
+                  <h3>
+                      <div className = "card-text-description-header py-2">
+                      {blog.title + " "}
+                      </div>
+          
+                      <hr/>
+                      <div>
+                      <Link to={`/blogs/${blog.id}/edit` } className = "link" id = {blog.id}><Button variant="success" id = {blog.id}>Edit</Button></Link>
+                      <Link to ={`/blogs/${blog.id}`}><Button variant="info" id = {blog.id} >Read</Button></Link>
+                      <Button variant="danger" id = {blog.id} onClick={this.handleClick}>Delete</Button>
+                      
+                      </div>
+                  </h3> 
+                  <p> 
+                      <div className="card-text-description px-3">
+                      {blog.description} 
+                      </div>
+                  </p>
+                  </div>
+                  
+              </Card> 
+          </div>
+          </div>)
+}  
+)
+
+
     const blogs = this.props.blogs.map((blog, i) => {
         return (
         <div className="center-card">
@@ -56,8 +103,16 @@ class BlogsIndex extends Component {
 
     return (
         <div className="px-3"  >
-           
+              
+                <Form onSubmit =  {this.someFunction}>
+                  <Button type ="submit"></Button>
+                  <input id = "someinput"></input>
+                </Form>
+              
+
               <h1>The Dunder Blog:</h1>
+
+              {blog}
               <br/>
               {this.props.loading ? <h3>Loading...</h3> : <CardDeck className="center-card"> {blogs} </CardDeck>}
           
--- src/reducers/jobReducer.js
+++ src/reducers/jobReducer.js
@@ -7,13 +7,11 @@ export default (state = {jobs: [], loading: false}, action) => {
         case("JOBS_LOADED"):
             return {...state, loading:false, jobs: action.payload}
 
-
         //CREATE
         case("ADDING_JOB"):
             return {...state, loading:false, job: action.payload}
         case("JOB_ADDED"):
             return {...state, loading:false, jobs: [...state.jobs, action.payload] }
-
 
         //EDIT
         case("EDITING_JOB"):
@@ -25,9 +23,8 @@ export default (state = {jobs: [], loading: false}, action) => {
         case("DELETING_JOB"):
             return{...state, loading:false, job: action.payload}
         case("JOB_DELETED"):
-        return {...state, loading:false, jobs: state.jobs.filter(job => job.id != action.payload) }
+            return {...state, loading:false, jobs: state.jobs.filter(job => job.id != action.payload) }
         
-
 
         //Like
         case("JOB_LIKED"):
